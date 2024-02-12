3a06a1c9b1f76e882bc882c73d8fb9c07824e996
aaronberkness <aberkness@csumb.edu>
2024-02-12 09:43:22 -0800

    Rough draft done. Just need to check what works and what needs fixing. For some reason ControllerPatientUpdate is not recognizing getConnection() which is weird.

--- src/main/java/application/ControllerPatientCreate.java
+++ src/main/java/application/ControllerPatientCreate.java
@@ -1,7 +1,7 @@
 package application;
 
+import java.sql.*;
 import java.sql.Connection;
-import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -19,11 +19,11 @@ import view.*;
  */
 @Controller
 public class ControllerPatientCreate {
-	
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
-	
+
+
 	/*
 	 * Request blank patient registration form.
 	 */
@@ -33,7 +33,7 @@ public class ControllerPatientCreate {
 		model.addAttribute("patient", new PatientView());
 		return "patient_register";
 	}
-	
+
 	/*
 	 * Process data from the patient_register form
 	 */
@@ -41,28 +41,67 @@ public class ControllerPatientCreate {
 	public String createPatient(PatientView p, Model model) {
 
 		/*
-		 * validate doctor last name and find the doctor id
-		 */
-		// TODO
-
-		/*
 		 * insert to patient table
 		 */
 
+		try (Connection con = getConnection();) {
+			PreparedStatement ps = con.prepareStatement("insert into patient(last_name, first_name, birthdate, ssn, street, city, state, zipcode, primaryName ) values(?, ?, ?, ?, ?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, p.getLast_name());
+			ps.setString(2, p.getFirst_name());
+			ps.setString(3, p.getBirthdate());
+			ps.setString(4, p.getSsn());
+			ps.setString(5, p.getStreet());
+			ps.setString(6, p.getCity());
+			ps.setString(7, p.getState());
+			ps.setString(8, p.getZipcode());
+			ps.setString(9, p.getPrimaryName());
 
-		// display patient data and the generated patient ID,  and success message
-		model.addAttribute("message", "Registration successful.");
-		model.addAttribute("patient", p);
-		return "patient_show";
+			// Check if Primary Name matches Doctor name before execute update.
+			// Note: the lab document gives the impression that primaryName is the doctor's last name.
 
-		/*
-		 * on error
-		 * model.addAttribute("message", some error message);
-		 * model.addAttribute("patient", p);
-		 * return "patient_register";
-		 */
+			/*
+			 * validate doctor last name and find the doctor id
+			 */
+
+			PreparedStatement docQ = con.prepareStatement("SELECT last_name from doctor");
+			ResultSet doc = docQ.executeQuery();
+
+			while (doc.next()) {
+				if (doc.getString(1).equals(p.getPrimaryName())) { // 				******
+
+					ps.executeUpdate();
+					ResultSet rs = ps.getGeneratedKeys();
+					if (rs.next()) p.setId(rs.getInt(1));
+
+					// display message and patient information
+					model.addAttribute("message", "Registration successful.");
+					model.addAttribute("patient", p);
+					//return "patient_show";
+					break;
+
+				} //																	*******
+			}
+
+			return "patient_show";
+			// return "invalid_doctor"; // Not sure if this is what should be returned when no match with a doctor.
+
+		} catch (SQLException e) {
+
+			/*
+			 * on error
+			 * model.addAttribute("message", some error message);
+			 * model.addAttribute("patient", p);
+			 * return "patient_register";
+			 */
+
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("patient", p);
+			return "patient_register";
+		}
+
 	}
-	
+
 	/*
 	 * Request blank form to search for patient by id and name
 	 */
@@ -71,7 +110,7 @@ public class ControllerPatientCreate {
 		model.addAttribute("patient", new PatientView());
 		return "patient_get";
 	}
-	
+
 	/*
 	 * Perform search for patient by patient id and name.
 	 */
@@ -79,12 +118,36 @@ public class ControllerPatientCreate {
 	public String showPatient(PatientView p, Model model) {
 
 		// TODO   search for patient by id and name
-		
 		// if found, return "patient_show", else return error message and "patient_get"
-		
-		return "patient_show";
+
+		try (Connection con = getConnection();) {
+
+			PreparedStatement ps = con.prepareStatement("select last_name, first_name, primaryName from patient where id=? and last_name=?");
+			ps.setInt(1, p.getId());
+			ps.setString(2, p.getLast_name());
+
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				p.setLast_name(rs.getString(1));
+				p.setFirst_name(rs.getString(2));
+				p.setPrimaryName(rs.getString(4));
+				model.addAttribute("patient", p);
+				return "patient_show";
+
+			} else {
+				model.addAttribute("message", "patient not found.");
+				model.addAttribute("patient", p);
+				return "patient_get";
+			}
+
+		} catch (SQLException e) {
+			System.out.println("SQL error in getPatient "+e.getMessage());
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("patient", p);
+			return "patient_get";
+		}
 	}
-	
+
 	/*
 	 * return JDBC Connection using jdbcTemplate in Spring Server
 	 */
--- src/main/java/application/ControllerPatientUpdate.java
+++ src/main/java/application/ControllerPatientUpdate.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import view.*;
+
+import java.sql.*;
+
+
 /*
  * Controller class for patient interactions.
  *   register as a new patient.
@@ -27,12 +31,48 @@ public class ControllerPatientUpdate {
 	public String getUpdateForm(@PathVariable int id, Model model) {
 
 		PatientView pv = new PatientView();
+
 		// TODO search for patient by id
 		//  if not found, return to home page using return "index"; 
 		//  else create PatientView and add to model.
 		// model.addAttribute("message", some message);
 		// model.addAttribute("patient", pv
 		// return editable form with patient data
+
+		pv.setId(id);
+
+		try (Connection con = getConnection();){
+
+			PreparedStatement ps = con.prepareStatement("SELECT last_name, first_name, birthdate, ssn, street, city, state, zipcode, primaryName from patient where id=?");
+			ps.setInt(1, id);
+
+			ResultSet rs = ps.executeQuery();
+
+			if (!rs.next()) {
+				return "index";
+			} else {
+				pv.setId(id);
+				pv.setLast_name(rs.getString(1));
+				pv.setFirst_name(rs.getString(2));
+				pv.setBirthdate(rs.getString(3));
+				pv.setSsn(rs.getString(4));
+				pv.setStreet(rs.getString(5));
+				pv.setCity(rs.getString(6));
+				pv.setState(rs.getString(7));
+				pv.setZipcode(rs.getString(8));
+				pv.setPrimaryName(rs.getString(9));
+
+				model.addAttribute("patient", pv);
+				return "patient_edit";
+			}
+
+		} catch (SQLException e) {
+			System.out.println("SQL error in getPatient "+e.getMessage());
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("patient", pv);
+			return "patient_get";
+		}
+
 		return "patient_edit";		 		
 }
 	
@@ -50,6 +90,64 @@ public class ControllerPatientUpdate {
 		// TODO 
 		
 		// TODO update patient profile data in database
+
+		// ---------------------------------------------------------------
+
+
+		try (Connection con = getConnection();) {
+			PreparedStatement ps = con.prepareStatement("update patient set street=?,  city=?, state=?, zipcode=?, primaryName=? where id=? ");
+			ps.setString(1, p.getStreet());
+			ps.setString(2, p.getCity());
+			ps.setString(3, p.getState());
+			ps.setString(4, p.getZipcode());
+			ps.setString(5, p.getPrimaryName());
+			ps.setInt(6, p.getId());
+
+			// Check if Primary Name matches Doctor name before execute update.
+			// Note: the lab document gives the impression that primaryName is the doctor's last name.
+
+			PreparedStatement docQ = con.prepareStatement("SELECT last_name from doctor");
+			ResultSet doc = docQ.executeQuery();
+
+			while (doc.next()) {
+				if (doc.getString(1).equals(p.getPrimaryName())) {
+
+					int rc = ps.executeUpdate();
+
+					if (rc == 1) {
+						model.addAttribute("message", "Update successful");
+						model.addAttribute("patient", p);
+						return "patient_show";
+
+					} else {
+						model.addAttribute("message", "Error. Update was not successful");
+						model.addAttribute("patient", p);
+						return "patient_edit";
+					}
+
+					break;
+
+				}
+			}
+
+			return "patient_show";
+			// return "invalid_doctor"; // Not sure if this is what should be returned when no match with a doctor.
+
+		} catch (SQLException e) {
+
+			/*
+			 * on error
+			 * model.addAttribute("message", some error message);
+			 * model.addAttribute("patient", p);
+			 * return "patient_register";
+			 */
+
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("patient", p);
+			return "patient_edit";
+		}
+
+		// ---------------------------------------------------------------
 
 		// model.addAttribute("message", some message);
 		// model.addAttribute("patient", p)
